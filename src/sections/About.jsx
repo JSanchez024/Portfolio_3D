a25126c9a8bf9cb6a0905b5f1f2d206880762e10
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2025-02-14 02:41:39 -0300

    v.1.7

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Globe from 'react-globe.gl'
 import { Button } from '../components/Button'
 
+
 export const About = () => {
 
     const deviceType = navigator.userAgent.match(/mobile|android|iphone|ipad|ipod|blackberry|windows phone/i) ? 'mobile' : 'desktop';
@@ -86,7 +87,7 @@ export const About = () => {
                 <div className='grid-container'>
                     <img src="/assets/grid3.png" alt="grid-3" className='w-full sm:h-[266px] h-fit object-contain' />
                     <div>
-                        <p className='grid-headtext'>Un poco sobre mi</p>
+                        <p className='grid-headtext'>Un poco sofsdfsdfbre mi</p>
                         <p className='grid-subtext'>Soy Tecnico Avansado en Programacion, formado en la Universidad Diego Portales.
                         Actualmente estoy en constante aprendisaje sobre desarrollo web y busco mi primera oportunidad para aplicar mis habilidades y conocimientos en un entorno profesional y aprender de un equipo experimentado.</p>
                     </div>
@@ -99,12 +100,16 @@ export const About = () => {
                     <img src="/assets/grid4.png" alt="grid-4" className='w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top' />
 
                     <div className='space-y-2'>
-                        <p className='text-white text-center animate-bounce mt-6'>joaquin.sanchez.suazo@gmail.com</p>
+                       { /*<p className='text-white text-center animate-bounce mt-6'>joaquin.sanchewrwerez.suazo@gmail.com</p>
                         <div className='copy-container' onClick={handleCopy}>
                             <img src={hasCopied ? '/assets/tick.svg' :
                                         '/assets/copy.svg'} alt="copy" className='mt-4' />
-
-                        </div>
+                            
+                        </div>*/}
+                       
+                       <div>
+                            <a href="/download/Joaquin_Sanchez_CV.pdf" download="Joaquin_Sanchez_cv.pdf" className='text-white' target="_blank" type='application/pdf'>Descargar</a>
+                       </div>
 
                     </div>
 
